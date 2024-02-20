6d2f912e5cdbe10ba8218660f12f722b003fb7f8
Dhruv <chodvadiyadhruv06@gmail.com>
2024-02-20 20:53:13 +0530

    Campaigns

--- src/main/java/com/Sale_Campaign/System/Service/CampaignsService.java
+++ src/main/java/com/Sale_Campaign/System/Service/CampaignsService.java
@@ -1,10 +1,12 @@
 package com.Sale_Campaign.System.Service;
 import com.Sale_Campaign.System.Repo.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Service;
 
 @EnableScheduling
 @Service
 public class CampaignsService {
-
+    @Autowired
+    ProductRepo productRepo;
 }
