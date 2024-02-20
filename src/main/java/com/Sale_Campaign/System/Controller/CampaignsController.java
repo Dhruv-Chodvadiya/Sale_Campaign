480eb6974c5a50c95409a1d82c15d978ca409bab
Dhruv <chodvadiyadhruv06@gmail.com>
2024-02-20 20:52:02 +0530

    Campaigns

---
+++ src/main/java/com/Sale_Campaign/System/Controller/CampaignsController.java
@@ -0,0 +1,12 @@
+package com.Sale_Campaign.System.Controller;
+import com.Sale_Campaign.System.Service.CampaignsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("CreateCampaign")
+public class CampaignsController {
+    @Autowired
+    CampaignsService campaignsService;
+}
---
+++ src/main/java/com/Sale_Campaign/System/Service/CampaignsService.java
@@ -0,0 +1,10 @@
+package com.Sale_Campaign.System.Service;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Service;
+
+@EnableScheduling
+@Service
+public class CampaignsService {
+
+}
