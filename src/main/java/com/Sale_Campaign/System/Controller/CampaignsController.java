57ae7628803af6ced7aeaade7c55f4e2392a305e
Dhruv <chodvadiyadhruv06@gmail.com>
2024-02-22 19:57:49 +0530

    API

--- src/main/java/com/Sale_Campaign/System/Controller/CampaignsController.java
+++ src/main/java/com/Sale_Campaign/System/Controller/CampaignsController.java
@@ -1,8 +1,12 @@
 package com.Sale_Campaign.System.Controller;
 import com.Sale_Campaign.System.Service.CampaignsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.ParseException;
 
 @RestController
 @RequestMapping("CreateCampaign")
@@ -10,4 +14,8 @@ public class CampaignsController {
     @Autowired
     CampaignsService campaignsService;
 
+//    @PostMapping("/campaign")
+//    public Campaigns campaigns(@RequestBody CompaignsDTO campaigns) throws ParseException {
+//        return campaignsService.createCampaigns(campaigns);
+//    }
 }
